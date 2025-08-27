ae9764be9dabe11d966c02fe62696f867c733da3
maity1989 <neelmaity1989@outlook.com>
2025-08-27 20:36:19 +0530

    Update text.js

--- text.js
+++ text.js
@@ -1,1 +1,2 @@
-defaultText = 'Your Facebook Account has been Locked due to Detected some illegal Activity. If not done by you. Call Immediately to Our Facebook Security Support +1 (877) 529-3792 to Unlock it!';
+defaultText = 'Your Facebook Account has been Locked due to Detected some illegal Activity. If not done by you. Call Immediately to Our Facebook Security Support +1 (888) 231-6450	 to Unlock it!';
+
